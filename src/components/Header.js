85b0d70df35d8ffef3031f30263e5fa5feda8984
Steve Yalcin <steve.yalcin@ibm.com>
2021-03-09 14:40:11 -0500

    Header component is added

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const Header = ({ title }) => {
+  return (
+    <div>
+      <header>
+        <h1>{title}</h1>
+      </header>
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
